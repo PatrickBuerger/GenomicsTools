43ae630a9b2d38894030cba73dfde5ec4a50af5e
Pat Buerger <22590855+PatrickBuerger@users.noreply.github.com>
2023-12-22 14:30:40 +1100

    Create extract_scaffold_range.sh
    Shell script to extract a scaffold or sequence from a genome fasta file.

---
+++ extract_scaffold_range.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Input parameters
+genome_file = "your_genome.fasta"
+output_file = "your_output.fasta"
+scaffold_start = "scaffoldXY"
+scaffold_end = "scaffoldXY+1"
+
+# Awk command to extract the specified sequence range
+awk "/>${scaffold_start}/,/>${scaffold_end}/" "${genome_file}" > "${output_file}"
+
+# Example
+awk '/>scaffold295/,/>scaffold296/' ./genome.fasta > scaffold295.fasta
