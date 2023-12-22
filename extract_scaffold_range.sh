3c8f3a4372e88b0fb5a93ea2032731ce72504e77
Pat Buerger <22590855+PatrickBuerger@users.noreply.github.com>
2023-12-22 14:53:39 +1100

    fixed input parameters

--- extract_scaffold_range.sh
+++ extract_scaffold_range.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Input parameters
-genome_file = "your_genome.fasta"
-output_file = "your_output.fasta"
-scaffold_start = "scaffoldXY"
-scaffold_end = "scaffoldXY+1"
+genome_file="your_genome.fasta"
+output_file="your_output.fasta"
+scaffold_start="scaffoldXY"
+scaffold_end="scaffoldXY+1"
 
 # Awk command to extract the specified sequence range
 awk "/>${scaffold_start}/,/>${scaffold_end}/" "${genome_file}" > "${output_file}"
