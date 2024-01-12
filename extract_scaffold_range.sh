0b2780e276290723f2a88ebe031a5d3005d26100
Pat Buerger <22590855+PatrickBuerger@users.noreply.github.com>
2024-01-12 16:02:52 +1100

    Update extract_scaffold_range.sh

--- extract_scaffold_range.sh
+++ extract_scaffold_range.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-# Input parameters
+## Input parameters
 genome_file="your_genome.fasta"
 output_file="your_output.fasta"
 scaffold_start="scaffoldXY"
 scaffold_end="scaffoldXY+1"
 
-# Awk command to extract the specified sequence range
+## Awk command to extract a scaffold from a genome fasta file
 awk "/>${scaffold_start}/,/>${scaffold_end}/" "${genome_file}" > "${output_file}"
 
-# Example
-awk '/>scaffold295/,/>scaffold296/' ./genome.fasta > scaffold295.fasta
+## Example
+# awk '/>scaffold295/,/>scaffold296/' ./genome.fasta > scaffold295.fasta
+
+
+## Alternative command using "seqkit"
+## Install: conda install -c bioconda seqkit
+## Seqkit website: https://bioinf.shenwei.me/seqkit/
+# seqkit grep -p ${scaffold_start} "${genome_file}" > "${output_file}"
+
